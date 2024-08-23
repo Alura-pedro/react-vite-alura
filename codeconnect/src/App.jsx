21fea83af6ba2d1a29ac95c7a196ef57385c8a2c
Pedro Henrique Martins Alves dos Santos <pedroshenriquepsantos@gmail.com>
2024-08-22 21:18:54 -0300

    a

--- codeconnect/src/App.jsx
+++ codeconnect/src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css';
 import Sidebar from './componentes/Sidebar';
-
+import BarraDePesquisa from './componentes/BarraDePesquisa';
 
 function App() {
   return (
-    <>
+    <div className='container'>
       <Sidebar/>
-    </>
+      <BarraDePesquisa/>
+    </div>
   )
 }
 
---
+++ codeconnect/src/componentes/BarraDePesquisa/index.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import './styles.css';
+
+function BarraDePesquisa() {
+    const [termoPesquisa, setTermoPesquisa] = useState('');
+    console.log(termoPesquisa);
+    return ( 
+        <input 
+        type = 'search' 
+        placeholder="Digite o que você procura" 
+        className='barra-pesquisa'
+        value={termoPesquisa}
+        onChange={(evento) => setTermoPesquisa(evento.target.value)}/>
+     );
+}
+
+export default BarraDePesquisa;
