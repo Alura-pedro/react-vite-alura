30f94b35898aab39eede42f813e49dd14e3c055a
Pedro Henrique Martins Alves dos Santos <pedroshenriquepsantos@gmail.com>
2024-09-01 20:47:07 -0300

    terminando a codeconnect

--- codeconnect/src/App.jsx
+++ codeconnect/src/App.jsx
@@ -2,14 +2,42 @@ import './App.css';
 import Sidebar from './componentes/Sidebar';
 import BarraDePesquisa from './componentes/BarraDePesquisa';
 import Filtro from './componentes/Filtro';
+import Card from './componentes/Card';
+import Ordenacao from './componentes/Ordenacao';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [dados, setDados] = useState([]);
+  useEffect(() => {
+    fetch('https://my-json-server.typicode.com/MonicaHillman/codeconnect-api/publicacoes')
+    .then(resposta => resposta.json())
+    .then(dados => setDados(dados))
+}, [])
+ 
   return (
     <div className='container'>
       <Sidebar/>
       <div>
         <BarraDePesquisa/>
         <Filtro/>
+        <Ordenacao/>
+        <ul className='lista-cards'>
+          {dados ? dados.map((item, index) => (
+            <li key={index}>
+              <Card
+              id={item.id}
+              imagemUrl={item.imagem}
+              titulo={item.titulo}
+              resumo={item.resumo}
+              linhasDeCodigo={item.linhas_de_codigo}
+              compartilhamentos={item.compartilhamentos}
+              comentarios={item.comentarios}
+              usuario={item.usuario}
+              />
+            </li>
+          )) : null}
+        </ul>
       </div>
     </div>
   )
---
+++ codeconnect/src/componentes/Ordenacao/index.jsx
@@ -0,0 +1,11 @@
+import './styles.css'
+
+export default function Ordenacao() {
+    return (
+        <div className="ordenacoes">
+            <a href="" className="link__1">Recentes</a>
+            <a href="" className="link__2">Recentes</a>
+        </div>
+    )
+
+}
